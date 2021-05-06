b3d57ac7a87ba3b3a65f14e910e311f22b1b2c16
Usaid M <UsaidPro@gmail.com>
2021-05-06 04:09:28 -0500

    Checking to see if preprocessing is fixed for Massachusetts

--- TopicModeling.py
+++ TopicModeling.py
@@ -34,12 +34,12 @@ for state in initial_state_list:
 
             count = 1
             for term in terms:
-                #term = re.sub(r'[.?!,:;()\-\n\d]', ' ', term)
-                #tokens = [t.lower() for t in word_tokenize(term) if t not in stopwords.words('english')]
+                term = re.sub(r'[.?!,:;()\-\n\d]', ' ', term)
+                tokens = [t.lower() for t in word_tokenize(term) if t not in stopwords.words('english')]
 
-                #wnl = WordNetLemmatizer()
-                #term = " ".join(wnl.lemmatize(t) for t in tokens)
-                term = " ".join(preprocess_string(term))
+                wnl = WordNetLemmatizer()
+                term = " ".join(wnl.lemmatize(t) for t in tokens)
+                #term_processed = " ".join(preprocess_string(term))
 
                 suggestions = sym_spell.lookup_compound(term, max_edit_distance=2)
                 corrected = " ".join([str(elem) for elem in suggestions])
@@ -57,9 +57,38 @@ for state in initial_state_list:
         state_list.append(state)
     except Exception:
         print('Issue with state ' + state)
-## Create a set of frequent words
-#stoplist = set('i for a of the and to in'.split(' '))
-## Lowercase each document, split it by white space and filter out stopwords
+# Create a set of frequent words
+STOPWORDS = frozenset([
+    'all', 'six', 'just', 'less', 'being', 'indeed', 'over', 'move', 'anyway', 'four', 'not', 'own', 'through',
+    'using', 'fifty', 'where', 'mill', 'only', 'find', 'before', 'one', 'whose', 'system', 'how', 'somewhere',
+    'much', 'thick', 'show', 'had', 'enough', 'should', 'to', 'must', 'whom', 'seeming', 'yourselves', 'under',
+    'ours', 'two', 'has', 'might', 'thereafter', 'latterly', 'do', 'them', 'his', 'around', 'than', 'get', 'very',
+    'de', 'none', 'cannot', 'every', 'un', 'they', 'front', 'during', 'thus', 'now', 'him', 'nor', 'name', 'regarding',
+    'several', 'hereafter', 'did', 'always', 'who', 'didn', 'whither', 'this', 'someone', 'either', 'each', 'become',
+    'thereupon', 'sometime', 'side', 'towards', 'therein', 'twelve', 'because', 'often', 'ten', 'our', 'doing', 'km',
+    'eg', 'some', 'back', 'used', 'up', 'go', 'namely', 'computer', 'are', 'further', 'beyond', 'ourselves', 'yet',
+    'out', 'even', 'will', 'what', 'still', 'for', 'bottom', 'mine', 'since', 'please', 'forty', 'per', 'its',
+    'everything', 'behind', 'does', 'various', 'above', 'between', 'it', 'neither', 'seemed', 'ever', 'across', 'she',
+    'somehow', 'be', 'we', 'full', 'never', 'sixty', 'however', 'here', 'otherwise', 'were', 'whereupon', 'nowhere',
+    'although', 'found', 'alone', 're', 'along', 'quite', 'fifteen', 'by', 'both', 'about', 'last', 'would',
+    'anything', 'via', 'many', 'could', 'thence', 'put', 'against', 'keep', 'etc', 'amount', 'became', 'ltd', 'hence',
+    'onto', 'or', 'con', 'among', 'already', 'co', 'afterwards', 'formerly', 'within', 'seems', 'into', 'others',
+    'while', 'whatever', 'except', 'down', 'hers', 'everyone', 'done', 'least', 'another', 'whoever', 'moreover',
+    'couldnt', 'throughout', 'anyhow', 'yourself', 'three', 'from', 'her', 'few', 'together', 'top', 'there', 'due',
+    'been', 'next', 'anyone', 'eleven', 'cry', 'call', 'therefore', 'interest', 'then', 'thru', 'themselves',
+    'hundred', 'really', 'sincere', 'empty', 'more', 'himself', 'elsewhere', 'mostly', 'on', 'fire', 'am', 'becoming',
+    'hereby', 'amongst', 'else', 'part', 'everywhere', 'too', 'kg', 'herself', 'former', 'those', 'he', 'me', 'myself',
+    'made', 'twenty', 'these', 'was', 'bill', 'cant', 'us', 'until', 'besides', 'nevertheless', 'below', 'anywhere',
+    'nine', 'can', 'whether', 'of', 'your', 'toward', 'my', 'say', 'something', 'and', 'whereafter', 'whenever',
+    'give', 'almost', 'wherever', 'is', 'describe', 'beforehand', 'herein', 'doesn', 'an', 'as', 'itself', 'at',
+    'have', 'in', 'seem', 'whence', 'ie', 'any', 'fill', 'again', 'hasnt', 'inc', 'thereby', 'thin', 'no', 'perhaps',
+    'latter', 'meanwhile', 'when', 'detail', 'same', 'wherein', 'beside', 'also', 'that', 'other', 'take', 'which',
+    'becomes', 'you', 'if', 'nobody', 'unless', 'whereas', 'see', 'though', 'may', 'after', 'upon', 'most', 'hereupon',
+    'eight', 'but', 'serious', 'nothing', 'such', 'why', 'off', 'a', 'don', 'whereby', 'third', 'i', 'whole', 'noone',
+    'sometimes', 'well', 'amoungst', 'yours', 'their', 'rather', 'without', 'so', 'five', 'the', 'first', 'with',
+    'make', 'once'
+])
+# Lowercase each document, split it by white space and filter out stopwords
 #texts = [[word for word in document.lower().split() if word not in stoplist]
 #         for document in all_terms]
 texts = []
@@ -71,8 +100,9 @@ for i in range(len(all_terms)):
     document_split = document.lower().split()
     labels_split = all_labels[i].split()
     for j in range(len(document_split)):
-        word_list.append(document_split[j])
-        label_list.append(labels_split[j])
+        if(document_split[j] not in STOPWORDS):
+            word_list.append(document_split[j])
+            label_list.append(labels_split[j])
     texts.append(word_list)
     labels.append(label_list)
 
